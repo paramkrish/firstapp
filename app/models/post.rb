cfe9b09e6d487d3115ff15734742795a9ae9684b
Param Krish <paramkrish@apples-MacBook-Pro.local>
2016-08-16 18:38:21 +0530

    added css

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
-	 validates :name , :presence => true
+#	 validates :name , :presence => true
+
 
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "null title insertion" do
+   	 post = Post.new(:name => "")
+     assert post.valid?
+   end
+
+   test "short title insertion" do
+   	 post = Post.new(:name => "Test")
+     assert post.valid?
+   end
+
+   test "long title insertion" do
+   	 post = Post.new(:name => "Test is really long title")
+     assert post.valid?
+   end
+
 end
